554e1b9ef92e9d9d1451b222b58ed383892b8168
JaoGallo <joaopaulogalloclaudino0907@gmail.com>
2026-05-04 16:23:33 +0000

    auth.php

--- config/auth.php
+++ config/auth.php
@@ -92,14 +92,17 @@ return [
     |
     */
 
-    'passwords' => [
-        'users' => [
-            'provider' => 'users',
-            'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
-            'expire' => 60,
-            'throttle' => 60,
-        ],
+'passwords' => [
+    'users' => [
+        'provider' => 'users',
+        'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
+        'expire' => 60,
+        'throttle' => 60,
     ],
+],
+
+'password_timeout' => env('AUTH_PASSWORD_TIMEOUT', 10800),
+
 
     /*
     |--------------------------------------------------------------------------
@@ -115,3 +118,4 @@ return [
     'password_timeout' => env('AUTH_PASSWORD_TIMEOUT', 10800),
 
 ];
+
